06392b21469e55f4842bf7f2adb1790146dd01c6
mayabres <110967883+mayabres@users.noreply.github.com>
2024-04-13 11:36:44 +0300

    update R file

--- פרויקט בויזואליזציה.R
+++ פרויקט בויזואליזציה.R
@@ -22,10 +22,9 @@ con <- dbConnect(MySQL(),
                  dbname = "visualization", 
                  host = "localhost",)
 
-##create the first net - before bitul ilat hasvirut
 table<-dbGetQuery(con,"select * from ages as a join eshkol as e on a.שם_ישוב=e.City_Name join religion as r on r.שם_יישוב =e.City_Name")
 # Sys.setlocale(category = "LC_ALL", locale = "Hebrew_Hebrew.65001")
 library(writexl)
-write_xlsx(table, "output_file.xlsx")
+write_xlsx(table, "output_file1.xlsx")
 
 
